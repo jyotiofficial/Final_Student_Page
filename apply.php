969818f793ebf2111a42327d4a1f87fd680146fc
Jyoti Anand Chandra Prasad <jyotianandchandraprasad@gmail.com>
2023-07-14 20:35:51 +0530

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	modified:   apply.php

--- apply.php
+++ apply.php
@@ -18,27 +18,66 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Initialize the announcement title variable
-$announcementTitle = '';
-
-// Retrieve the announcement titles and cv_file values from the tables
-$sql = "SELECT SUBSTRING_INDEX(SUBSTRING_INDEX(a.cv_file, '_', -2), '_', 1) AS announcement_title, a.cv_file
-        FROM applications AS a
-        INNER JOIN new_annoucement AS n ON n.announcement_title = SUBSTRING_INDEX(SUBSTRING_INDEX(a.cv_file, '_', -2), '_', 1)";
+// Retrieve the announcement titles from the new_annoucement table
+$sql = "SELECT announcement_title FROM new_annoucement";
 $result = $conn->query($sql);
 
-// Check if there are any matching rows
+// Check if there are any announcement titles
 if ($result && $result->num_rows > 0) {
+    // Fetch the announcement titles one by one
     while ($row = $result->fetch_assoc()) {
         $announcementTitle = $row['announcement_title'];
-        $cvFile = $row['cv_file'];
 
         // Update the company_name column in the applications table with the announcement title
-        $updateSql = "UPDATE applications SET company_name=? WHERE cv_file=?";
+        $updateSql = "UPDATE applications SET company_name = SUBSTRING_INDEX(SUBSTRING_INDEX(cv_file, '_', -2), '_', 1) WHERE cv_file LIKE CONCAT('%', ?, '%')";
         $stmtUpdate = $conn->prepare($updateSql);
-        $stmtUpdate->bind_param("ss", $announcementTitle, $cvFile);
+        $stmtUpdate->bind_param("s", $announcementTitle);
         $stmtUpdate->execute();
         $stmtUpdate->close();
+    }
+}
+
+// Prepare the success and error message variables
+$successMessage = "";
+$errorMessage = "";
+
+// Check if the form is submitted
+if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["submit"])) {
+    $userName = $_POST["userName"];
+    $admissionNo = $_POST["admissionNo"];
+    $contactNo = $_POST["Contact"];
+    $studentLocation = $_POST["StudentLocation"];
+    $resume = $_FILES["resume"];
+
+    // Check if a file is selected
+    if (isset($resume) && $resume["error"] === UPLOAD_ERR_OK) {
+        // Specify the target directory to store the uploaded files
+        $targetDirectory = __DIR__ . "/CV_Uploads/";
+
+        // Generate a unique filename based on the given format
+        $filename = $userName . "_" . $announcementTitle . "_" . $admissionNo . ".pdf";
+
+        // Move the uploaded file to the target directory
+        if (move_uploaded_file($resume["tmp_name"], $targetDirectory . $filename)) {
+            // Update the company_name column in the applications table with the announcement title
+            $companyName = substr($filename, strpos($filename, "_") + 1, strrpos($filename, "_") - strpos($filename, "_") - 1);
+
+            // Insert the data into the "Applications" table
+            $sql = "INSERT INTO Applications (student_name, admission_no, contact_no, student_location, cv_file, company_name, application_date) VALUES (?, ?, ?, ?, ?, ?, NOW())";
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param("ssssss", $userName, $admissionNo, $contactNo, $studentLocation, $filename, $companyName);
+            $stmt->execute();
+            $stmt->close();
+
+            // Display success message
+            $successMessage = "Applying for " . $companyName . " has been successful.";
+        } else {
+            // Display error message
+            $errorMessage = "Failed to move the uploaded file.";
+        }
+    } else {
+        // Display error message
+        $errorMessage = "Please select a valid PDF file.";
     }
 }
 
@@ -58,17 +97,19 @@ $conn->close();
     <div class="container my-3" id="content">
         <div class="container my-3 text-justify" id="content">
             <div class="bg-light p-5 rounded">
-                <?php if (isset($successMessage)) : ?>
+                <?php if (!empty($successMessage)) : ?>
                     <div class="alert alert-success" role="alert">
                         <?php echo $successMessage; ?>
                     </div>
-                <?php elseif (isset($errorMessage)) : ?>
+                <?php endif; ?>
+
+                <?php if (!empty($errorMessage)) : ?>
                     <div class="alert alert-danger" role="alert">
                         <?php echo $errorMessage; ?>
                     </div>
-                <?php endif; ?>  
+                <?php endif; ?>
 
-                <form class="row g-3" action="<?php echo htmlentities($_SERVER['PHP_SELF']) ?>" method="POST" enctype="multipart/form-data">
+                <form class="row g-3" action="<?php echo htmlentities($_SERVER["PHP_SELF"]); ?>" method="POST" enctype="multipart/form-data">
                     <div class="col-12">
                         <strong for="userName" class="form-label">Student Full Name</strong>
                         <input type="text" class="form-control" spellcheck="false" required autocomplete="off" name="userName" id="userName" placeholder="John Richard Doe">
@@ -95,9 +136,9 @@ $conn->close();
                                 <i>
                                     CV format
                                     <br>
-                                    <b class="text-danger bg-warning">Student-name_Company-name_Admission-no.pdf</b>
+                                    <b class="text-danger bg-warning">Student-name_Announcement-title_Admission-no.pdf</b>
                                     <br>
-                                    (JohnDoe_MarkIndustries_2000PE0400.pdf)
+                                    (JohnDoe_<?php echo $announcementTitle; ?>_2000PE0400.pdf)
                                 </i>
                             </small>
                         </div>
