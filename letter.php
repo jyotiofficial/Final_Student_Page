c752779d99cb7e04c042d33b40c8cb6c03e157d1
Jyoti Anand Chandra Prasad <jyotianandchandraprasad@gmail.com>
2023-07-22 17:19:54 +0530

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	new file:   letter.php 	modified:   previous.php

---
+++ letter.php
@@ -0,0 +1,117 @@
+<?php
+require '../../Libraries/fpdf/fpdf.php';
+
+// Replace these with your database credentials
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "internship_portal";
+
+// Create a database connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check if the connection is successful
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Fetch the value of 'name' from the 'student' table in attribute 's_name'
+$tableName = 'student';
+$columnName = 's_name';
+$alias = 'name';
+$query = "SELECT $columnName AS $alias FROM $tableName";
+$result = mysqli_query($conn, $query);
+
+if ($result) {
+    $row = mysqli_fetch_assoc($result);
+    $studentName = $row[$alias]; // Fetch the 'name' as 's_name'
+} else {
+    echo "Failed to fetch student name.";
+}
+
+// Fetch the value of 'application_date' from table 'applications'
+$tableName = 'applications';
+$columnName = 'application_date';
+$alias = 'application_date';
+$query = "SELECT $columnName AS $alias FROM $tableName";
+$result = mysqli_query($conn, $query);
+
+if ($result) {
+    $row = mysqli_fetch_assoc($result);
+    $applicationDate = $row[$alias]; // Fetch the 'application_date'
+} else {
+    echo "Failed to fetch application date.";
+}
+
+// Fetch data from the database
+$sql = "SELECT * FROM internship_applications ORDER BY ID DESC LIMIT 1";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+
+    // Extract values from the fetched data
+    $refrenceNumber = "CE/INTERN/" . sprintf("%04d", intval($row['ID']) + 1) . "/" . date('Y') . "-" . (date('y') + 1);
+    $date = $applicationDate;
+    $name = $studentName; // Using the fetched student name
+    $applicationID = $row['ID'];
+    $start_date = $row['startDate'];
+    $end_date = $row['endDate'];
+    $year = $row['created_at'];
+    $branch = $row['branch'];
+    $academicYear = $row['semester'];
+    $company = $row['CompanyName'];
+    $companyaddress = $row['CompanyAddress'];
+
+    $pdf = new FPDF('P', 'mm', 'Letter');
+    $pdf->SetLeftMargin(30);
+    $pdf->SetRightMargin(30);
+    $pdf->SetTopMargin(40);
+    $pdf->AddPage();
+    $pdf->SetFont('Times', '');
+    $pdf->Cell(70, 20, "Ref. No.:" . $refrenceNumber, 0, 0, "L");
+    $pdf->Cell(90, 20, $date, 0, 1, "R");
+    $pdf->SetFont('Times', 'B');
+    $pdf->Cell(60, 6, "Manager", 0, 1, "L");
+    $pdf->SetFont('Times', '');
+    $pdf->Cell(60, 6, $company, 0, 1, "L");
+    $pdf->MultiCell(65, 6, $companyaddress . ",", 0, "L");
+    $pdf->SetFont('Times', 'B');
+    $pdf->Cell(0, 5, "", 0, 1);
+    $pdf->Cell(50, 15, "Subject :", 0, 0, "R");
+    $pdf->SetFont('Times', 'BU');
+    $pdf->Cell(80, 15, "Permission for Internship Training.", 0, 1, "L");
+    $pdf->SetFont('Times', '');
+    $pdf->Cell(70, 15, "Dear Sir,", 0, 1, "L");
+
+    $pdf->Write(8, "With reference to above subject we request you to permit our student ");
+    $pdf->SetFont('Times', 'B');
+    $pdf->Write(8, $name);
+    $pdf->SetFont('Times', '');
+    $pdf->Write(8, " , who have appeared for " . $year . " ");
+    $pdf->SetFont('Times', 'B');
+    $pdf->Write(8, $branch);
+    $pdf->SetFont('Times', '');
+    $pdf->Write(8, " examinations during a.y. " . $academicYear . " to undertake internship training in your esteemed organization during their vacation ");
+    $pdf->SetFont('Times', '');
+    $pdf->Write(8, $start_date . " to " . $end_date);
+    $pdf->SetFont('Times', '');
+    $pdf->Write(8, " and also on Saturdays, Sundays and Public Holidays, as the case may be.");
+    $pdf->Cell(0, 20, "", 0, 1);
+    $pdf->Write(8, "We will be grateful if your esteemed organization would help us to provide practical training for our student.");
+    $pdf->Cell(0, 15, "", 0, 1);
+    $pdf->Write(8, "This certificate is issued on request of student for Internship purpose.");
+    $pdf->Cell(0, 15, "", 0, 1);
+
+    $pdf->Cell(0, 10, "Thank you.", 0, 1);
+    $pdf->Cell(0, 20, "Yours faithfully", 0, 1);
+
+    // Output the PDF inline
+    $pdf->Output("I", "Intern_Application_" . $applicationID);
+} else {
+    echo "No data found in the database.";
+}
+
+// Close the database connection
+$conn->close();
+?>
